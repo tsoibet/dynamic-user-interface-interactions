7013c4dec8a9a2afdf42f7a34fab48730dcde663
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-11-26 00:28:29 +0900

    Implement drop-down menu

---
+++ dropDownMenu.js
@@ -0,0 +1,32 @@
+function showMenu() {
+  const menuContent = document.querySelector('.dropDownMenuContent');
+  menuContent.classList.remove('invisible');
+}
+
+function hideMenu() {
+  const menuContent = document.querySelector('.dropDownMenuContent');
+  menuContent.classList.add('invisible');
+}
+
+export default function writeMenuDOM(buttonTitle, optionList) {
+  const container = document.querySelector('#dropDownMenu');
+  const menuButton = document.createElement('div');
+  menuButton.classList.add('dropDownMenuButton');
+  menuButton.textContent = buttonTitle;
+  container.appendChild(menuButton);
+  const menuContent = document.createElement('div');
+  menuContent.classList.add('dropDownMenuContent');
+  menuContent.classList.add('invisible');
+  for (const option of optionList) {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('dropDownMenuItem');
+    menuItem.textContent = option;
+    menuItem.addEventListener('click', () => {
+      alert(`You clicked ${option} !`);
+    });
+    menuContent.appendChild(menuItem);
+  }
+  container.appendChild(menuContent);
+  menuButton.addEventListener('mouseenter', showMenu);
+  container.addEventListener('mouseleave', hideMenu);
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+import writeMenuDOM from './dropDownMenu.js';
+
+writeMenuDOM('Drop-down', ['Option 1', 'Option 22', 'Option 333']);
