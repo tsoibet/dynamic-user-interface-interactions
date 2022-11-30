d7c3227714e2c1a66991ad00640230d93f138be9
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-12-01 00:14:05 +0900

    Reorganize dropDownMenu codes

--- dropDownMenu.js
+++ dropDownMenu.js
@@ -1,13 +1,3 @@
-function showMenu(id) {
-    const menuContent = document.querySelector(`#${id}`);
-    menuContent.classList.remove('invisible');
-}
-
-function hideMenu(id) {
-    const menuContent = document.querySelector(`#${id}`);
-    menuContent.classList.add('invisible');
-}
-
 export default function createDropdownMenu(parent, target, buttonTitle, optionList) {
     const container = document.querySelector(parent);
     const menu = document.createElement('div');
@@ -17,26 +7,21 @@ export default function createDropdownMenu(parent, target, buttonTitle, optionLi
     menuButton.textContent = buttonTitle;
     menu.appendChild(menuButton);
     const menuContent = document.createElement('div');
-    menuContent.id = `${buttonTitle}-list`;
     menuContent.classList.add('dropDownMenuContent');
-    menuContent.classList.add('invisible');
     for (const option of optionList) {
         const menuItem = document.createElement('div');
         menuItem.classList.add('dropDownMenuItem');
         menuItem.textContent = option;
         menuItem.addEventListener('click', () => {
             menuItemOnClick(option, target);
-            hideMenu(menuContent.id);
+            menuContent.style.display = 'none';
+            setTimeout(() => {
+                menuContent.style.display = '';
+            }, 1);
         });
         menuContent.appendChild(menuItem);
     }
     menu.appendChild(menuContent);
-    menuButton.addEventListener('mouseenter', () => {
-        showMenu(menuContent.id);
-    });
-    menu.addEventListener('mouseleave', () => {
-        hideMenu(menuContent.id);
-    });
     container.appendChild(menu);
 }
 
